492ec4f0c042d43feb5ee0886ee50a397f7250e4
AlexisFarfan <jumpytunechi@gmail.com>
2021-11-08 15:06:48 +0100

    Creación de nuevos prefabs
    creación de prefabs para decorar.

---
+++ PR_ZAXXON_FarfanAlexis/Assets/Scripts/CrearRoscas.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearRoscas : MonoBehaviour
+{
+    [SerializeField] GameObject MyPrefabs;
+    [SerializeField] Transform PosicionInicial;
+    Vector3 NewPos;
+    float randomZ;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Bucle");
+    }
+
+    void CreaRoscas()
+    {
+        
+        Vector3 initPos = new Vector3(0, 0, 0);
+        Vector3 NewPos = PosicionInicial.position + initPos;
+
+        //que no lo gire
+        Instantiate(MyPrefabs, NewPos, Quaternion.identity);
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+    IEnumerator Bucle()
+    {
+        while (true)
+        {
+
+
+            CreaRoscas();
+            yield return new WaitForSeconds(0.3f);
+
+
+        }
+
+
+    }
+}
---
+++ PR_ZAXXON_FarfanAlexis/Assets/Scripts/Rosca_Mover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rosca_Mover : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    float posZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = speed + 0.1f;
+
+        posZ = transform.position.z;
+
+        if (posZ < -20)
+        {
+            Destroy(gameObject);
+
+        }
+
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
+    }
+}
