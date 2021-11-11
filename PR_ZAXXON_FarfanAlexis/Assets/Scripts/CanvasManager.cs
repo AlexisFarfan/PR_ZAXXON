30876c4288965e6ba78a2928f26be5cefcf20e36
AlexisFarfan <jumpytunechi@gmail.com>
2021-11-11 20:21:53 +0100

    Asignación de vidas
    creando el scripts de las vidas y asociandolas a un sprite distinto.

---
+++ PR_ZAXXON_FarfanAlexis/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+
+    [SerializeField] Image livesImage;
+    [SerializeField] Sprite[] livesSprites;
+
+    [SerializeField] int Lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesImage.sprite = livesSprites[Lives];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //livesImage.sprite = livesSprites[Lives];
+    }
+}
